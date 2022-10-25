bbd4939a1d21a0d653710f0f84f8c08f83ba827e
Shivam Sharma <shivam691999@gmail.com>
2022-10-26 04:26:36 +0530

    add : block level miner detail

--- index.js
+++ index.js
@@ -24,36 +24,58 @@ async function getMiner(blockNum) {
 
 async function main() {
 
+    let now = Math.floor(new Date().getTime() / 1000)
+
+    await fs.appendFile(`./output/outFull-${now}.csv`, `timestamp, blockNumber, Validator, TxLength`, function (err) {
+        if (err) throw err;
+        console.log('Created output file : ' + `./output/outFull-${now}.csv`);
+    });
+
+    await timer(1000)
+
     let endblock = await web3.eth.getBlockNumber()
     let startBlock = endblock - 50000;
     var totalBlocks = new Map();
     var emptyBlocks = new Map();
 
-    for (let i = startBlock; i < endblock; i++) {
-        if(i%100===0){
+    for (let i = startBlock; i < endblock;) {
+        if (i % 100 === 0) {
             console.log("Block: " + i);
         }
-        var blockObject = await web3.eth.getBlock(i);
-        var allTransactions = blockObject.transactions;
-        var miner = await getMiner(i)
 
-        if(totalBlocks.has(miner)){
-            totalBlocks.set(miner, parseInt(totalBlocks.get(miner))+1)
-            if(allTransactions.length===0){
-                emptyBlocks.set(miner, parseInt(emptyBlocks.get(miner))+1)
+        try {
+            var blockObject = await web3.eth.getBlock(i);
+            var allTransactions = blockObject.transactions;
+            var miner = await getMiner(i)
+
+            if (totalBlocks.has(miner)) {
+                totalBlocks.set(miner, parseInt(totalBlocks.get(miner)) + 1)
+                if (allTransactions.length === 0) {
+                    emptyBlocks.set(miner, parseInt(emptyBlocks.get(miner)) + 1)
+                }
+            } else {
+                totalBlocks.set(miner, 1)
+                emptyBlocks.set(miner, 0)
+                if (allTransactions.length === 0) {
+                    emptyBlocks.set(miner, 1)
+                }
             }
-        }else{
-            totalBlocks.set(miner, 1)
-            emptyBlocks.set(miner, 0)
-            if(allTransactions.length===0){
-                emptyBlocks.set(miner, 1)
-            }
-        }
+
+            await fs.appendFile(`./output/outFull-${now}.csv`, `${blockObject.timestamp}, ${i}, ${miner}, ${allTransactions.length}`, function (err) {
+                if (err) throw err;
+            });
+
+            i++;
+        } catch (error) {
+            console.log("Warn: " + error)
+            console.log("Retrying block: " + i)
+            timer(1000)
+        }   
+
     }
 
-    let now = Math.floor(new Date().getTime() / 1000)
 
-    await fs.appendFile(`./output/out-${now}.csv`, `validator, total-blocks, zero-blocks` , function (err) {
+    await fs.appendFile(`./output/out-${now}.csv`, `validator, total-blocks, zero-blocks`, function (err) {
         if (err) throw err;
         console.log('Created output file : ' + `./output/out-${now}.csv`);
     });
@@ -62,7 +84,7 @@ async function main() {
 
 
     for (var [key, value] of totalBlocks.entries()) {
-        await fs.appendFile(`./output/out-${now}.csv`, `\n${key}, ${value}, ${emptyBlocks.get(key)}` , function (err) {
+        await fs.appendFile(`./output/out-${now}.csv`, `\n${key}, ${value}, ${emptyBlocks.get(key)}`, function (err) {
             if (err) throw err;
 
         });
