a7917b6ba3cc96d21bccc36d7bc735b17044eb90
Shivam Sharma <shivam691999@gmail.com>
2022-10-26 23:28:59 +0530

    chg : minor fix

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function main() {
         } catch (error) {
             console.log("Warn: " + error)
             console.log("Retrying block: " + i)
-            timer(1000)
+            await timer(1000)
         }   
 
     }
